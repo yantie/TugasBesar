131540d5716e171009086720102daa2413bafbf0
yantie <yantiezn5@gmail.com>
2014-12-19 05:49:01 +0700

    commit pertama

---
+++ src/buku_bu_diann/Buku_bu_diann.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buku_bu_diann;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Yanti
+ */
+public class Buku_bu_diann {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException{
+        // TODO code application logic here
+        screenn sc = new screenn();
+        sc.setLocationRelativeTo(null);
+        sc.setVisible(true);
+    }
+}
